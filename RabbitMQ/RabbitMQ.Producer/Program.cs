bc1835e98e9b11d14f7bedb2f18be86794409c6a
Nigar Orucova <92165280+Norucova@users.noreply.github.com>
2023-02-15 11:07:51 +0400

    feat: added project

---
+++ RabbitMQ/RabbitMQ.Producer/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+
+
+const string queue_name = "code-queue";
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: queue_name,
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+
+    );
+
+int i = 1;
+while (i<int.MaxValue)
+{
+
+    var message = new { Name = "Producer", Message = $"Console Message -> Code Academy", MessageID = i };
+    i++;
+
+    var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+    channel.BasicPublish("", queue_name, null, body);
+    Console.WriteLine($"{i,-10} Numarali mesaj gonderildi ");
+}
+
+
+//docker-compose up -d
